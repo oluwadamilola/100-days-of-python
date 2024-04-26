03b0069ee9cb5cc12672296fb4041204877c3cd9
oluwadamilola <dammyolukoju@gmail.com>
2024-04-26 15:25:23 +0100

    Bill calculator

--- Day-02/main.py
+++ Day-02/main.py
@@ -1,10 +1,10 @@
-hieght = 1.2
-weight = 80
-iswinning = True
-
-print (f"your height {hieght}")
-
-age = input()
-years = 90 - int(age)
-weeks = years * 52
-print(f"you have {weeks} weeks left")
+print("Welcome to tip calculator")
+bill = float(input( "what was the total bill\n"))
+tip = int(input("how much tip you will like to give 10, 15, 20 \n"))
+people = int(input("how many will you like to split with \n"))
+tip_as_percent = tip/100
+totat_tip_amount = tip_as_percent* bill
+total_bill = bill + totat_tip_amount
+bill_per_person = total_bill/people
+final_amount = round(bill_per_person, 2)
+print(f" Each person should pay {final_amount}")
