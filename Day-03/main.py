e6a84ab20ee8a4827d2a16afe0b47aa5f821220b
oluwadamilola <dammyolukoju@gmail.com>
2024-04-26 15:45:05 +0100

    comparison operator in python

---
+++ Day-03/main.py
@@ -0,0 +1,6 @@
+print("welcome to the roller coaster")
+height = int(input("whats your heigh\n"))
+if height >= 120:
+ print("you can ride the roller coaster")
+else:
+    print("you cannot ride the roller coaster")
