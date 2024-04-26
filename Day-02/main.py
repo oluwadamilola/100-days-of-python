c5c2094727ba959ef51828060c0c11de623d2edf
oluwadamilola <dammyolukoju@gmail.com>
2024-04-26 15:29:29 +0100

    refactor bill calculator

--- Day-02/main.py
+++ Day-02/main.py
@@ -6,5 +6,5 @@ tip_as_percent = tip/100
 totat_tip_amount = tip_as_percent* bill
 total_bill = bill + totat_tip_amount
 bill_per_person = total_bill/people
-final_amount = round(bill_per_person, 2)
-print(f" Each person should pay {final_amount}")
+final_amount ="{:2f}".format(bill_per_person)
+print(f" Each person should pay:  ${final_amount}")
