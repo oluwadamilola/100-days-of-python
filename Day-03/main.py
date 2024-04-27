83546f2e69f8241b10bdd92205df4f22bbe38e99
oluwadamilola <dammyolukoju@gmail.com>
2024-04-27 23:28:36 +0100

    bmi calculator mini project

--- Day-03/main.py
+++ Day-03/main.py
@@ -1,11 +1,9 @@
-print("welcome to the roller coaster")
-height = int(input("whats your heigh\n"))
-if height >= 120:
- print("you can ride the roller coaster")
- age =int(input("what is your age"))
- if age <=18:
-    print("please pay $7")
- else:
-    print("please pay $12")
-else:
- print('cant ride ')
+print("BMI calculator")
+
+height = float(input("whats your height"))
+weight = float(input(" what your weight"))
+bmi = weight/height*height
+if bmi < 18.5:
+    print(f"here is your {bmi} you are under weight" )
+elif bmi > 30.6:
+    print(f"here is your bmi {bmi} you are obese")
