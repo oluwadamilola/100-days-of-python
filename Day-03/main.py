a39261c9daca7b767165aca33c57e3af8467c83c
oluwadamilola <dammyolukoju@gmail.com>
2024-04-27 23:18:19 +0100

    Nexted if in python

--- Day-03/main.py
+++ Day-03/main.py
@@ -2,5 +2,10 @@ print("welcome to the roller coaster")
 height = int(input("whats your heigh\n"))
 if height >= 120:
  print("you can ride the roller coaster")
+ age =int(input("what is your age"))
+ if age <=18:
+    print("please pay $7")
+ else:
+    print("please pay $12")
 else:
-    print("you cannot ride the roller coaster")
+ 1print('cant ride ')
