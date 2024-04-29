b77c30c6ffc64e8c024633ebf5655aadafc5bc9d
oluwadamilola <dammyolukoju@gmail.com>
2024-04-29 12:16:34 +0100

    trasure game

--- Day-03/main.py
+++ Day-03/main.py
@@ -1,9 +1,7 @@
-print("BMI calculator")
-
-height = float(input("whats your height"))
-weight = float(input(" what your weight"))
-bmi = weight/height*height
-if bmi < 18.5:
-    print(f"here is your {bmi} you are under weight" )
-elif bmi > 30.6:
-    print(f"here is your bmi {bmi} you are obese")
+print("welcome to treasure island")
+print ("your mission is to find the treasure")
+choice1 = input('you\'re at a crossroad, where do you want to go? Type Left or Right ').lower()
+if choice1 == "left":
+    choice2 = input('you have come into a lake. there is an island in the middle of the lake. type "wait" or type "swim" to swim accross.').lower()
+else:
+    print("you fell into a hole, Game Over")
