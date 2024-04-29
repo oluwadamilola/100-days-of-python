7110f5d4f7660a5b9403d29af56f7adc267ab51f
oluwadamilola <dammyolukoju@gmail.com>
2024-04-29 12:24:46 +0100

    refactor the treasure game

--- Day-03/main.py
+++ Day-03/main.py
@@ -3,5 +3,13 @@ print ("your mission is to find the treasure")
 choice1 = input('you\'re at a crossroad, where do you want to go? Type Left or Right ').lower()
 if choice1 == "left":
     choice2 = input('you have come into a lake. there is an island in the middle of the lake. type "wait" or type "swim" to swim accross.').lower()
+if choice2 == "wait":
+    choice3 = input('you arrive the highland unharmed, There is a house with 3 door one red, one blue, one yellow which colour do you choose').lower()
+    if choice3 == "red":
+        print('its a room full of fire: Game Over')
+    elif choice3 =="yellow":
+        print("congratulation you found the treasure")
+    elif choice3 ==" blue":
+        print("you enter a room full of beast: Game Over")
 else:
     print("you fell into a hole, Game Over")
