c8fea5c11c2ae949575ba200160004d00c6d7196
oluwadamilola <dammyolukoju@gmail.com>
2024-05-19 12:47:26 +0100

    feature(password generator)

--- Day-05/password.py
+++ Day-05/password.py
@@ -8,16 +8,22 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = ""
+password_list =[]
 
 for char in range(1, nr_letters + 1):
-     password += random.choice(letters)
+     password_list.append(random.choice(letters))
  
 for char in range(1, nr_numbers + 1):
-     password += random.choice(numbers)
+     password_list += random.choice(numbers)
 
 for char in range(1, nr_symbols + 1):
-     password += random.choice(symbols)
-     print(password)
+     password_list += random.choice(symbols)
+
+     print(password_list)
  
- 
+     password_list = random.shuffle(password_list)
+     print(password_list)
+     password = ""
+     for char in password_list:
+          password += char
+          print(f"your password is {password}")
